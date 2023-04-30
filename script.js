a2776e749ea28d68a4730339766c5ba4e11566e1
fabiolopes22103261 <67275812+fabiofalopes@users.noreply.github.com>
2023-04-30 14:49:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+//print width and height of the window
+console.log("Width = " + window.innerWidth + " Height = " + window.innerHeight);
+
+//animate the image inside the nav bar
+var img = document.getElementById("profilePicture");
+img.style.position = "relative";
+img.style.left = "0px";
+img.style.top = "0px";
